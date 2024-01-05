45d2e9d00400dab0f4ed4fbcdf9411234feeba7f
Tigran Rostomyan <rostomyan.tm@gmail.com>
2024-01-05 18:07:40 +0200

    ierate by pages

--- main.go
+++ main.go
@@ -41,62 +41,89 @@ type Owner struct {
 	Link         string `json:"link"`
 }
 
-func GetQuestinsByDateRange(start, stop time.Time) Response {
+func GetQuestinsPageInDateRange(start, stop time.Time, page int) (Response, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 	req, err := http.NewRequestWithContext(
 		ctx,
 		"GET",
 		fmt.Sprintf(
-			"https://api.stackexchange.com/2.3/questions?order=desc&min=%d&max=%d&sort=creation&site=stackoverflow",
+			"https://api.stackexchange.com/2.3/questions?page=%d&order=desc&min=%d&max=%d&sort=creation&site=stackoverflow",
+			page,
 			start.Unix(),
 			stop.Unix(),
 		),
 		nil,
 	)
 	if err != nil {
-		panic(err)
+		return Response{}, err
 	}
 
 	client := http.DefaultClient
 	res, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return Response{}, err
 	}
 
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		return Response{}, err
 	}
 
 	var response Response
 	err = json.Unmarshal(data, &response)
 	if err != nil {
-		panic(err)
+		return Response{}, err
 	}
 
 	// fmt.Printf("%+v\n", response)
 	if len(response.Items) == 0 {
 		fmt.Printf("%#v\n", response)
+		return Response{}, fmt.Errorf("Response with 0 items")
 	}
 
 	fmt.Printf(
-		"got %d questions (1st ID: %d) for time range %s - %s. quota remaining: %d\n",
+		"[GetQuestinsPageInDateRange] got %d questions (1st ID: %d) for time range %s - %s. quota remaining: %d\n",
 		len(response.Items),
 		response.Items[0].QuestionId,
 		start.Format(time.DateOnly),
 		stop.Format(time.DateOnly),
 		response.QuotaRemaining,
 	)
-	return response
+	return response, nil
+}
+
+func GetQuestinsByDateRange(minDate, maxDate time.Time) ([]Question, error) {
+	questions := []Question{}
+
+	// Get 1st page for that date range
+	result, err := GetQuestinsPageInDateRange(minDate, maxDate, 1)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get all other pages if any
+	for page := 2; result.HasMore; page++ {
+		result, err = GetQuestinsPageInDateRange(minDate, maxDate, page)
+		if err != nil {
+			return nil, err
+		}
+
+		questions = append(questions, result.Items...)
+	}
+
+	return questions, nil
 }
 
 func main() {
 	maxDate := time.Now()
 	minDate := maxDate.Add(-24 * time.Hour)
-	for {
-		GetQuestinsByDateRange(minDate, maxDate)
-		maxDate = minDate
-		minDate = minDate.Add(-24 * time.Hour)
+	questions, err := GetQuestinsByDateRange(minDate, maxDate)
+	if err != nil {
+		panic(err)
+	}
+
+	for i, q := range questions {
+		fmt.Printf("%d\t %s", i+1, q.Title)
 	}
 }
